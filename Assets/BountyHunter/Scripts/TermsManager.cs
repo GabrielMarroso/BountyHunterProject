b4020db9bdecdfbd5d34bca071afb185c0d6a80f
Gabriel Marroso <gabrielmarroso@gmail.com>
2024-09-07 14:22:30 -0300

    Terms

---
+++ Assets/BountyHunter/Scripts/TermsManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TermsManager : MonoBehaviour
+{
+    public void OpenTerms()
+    {
+        Application.OpenURL("https://cottony-random-527.notion.site/Terms-of-Service-and-Conditions-of-Use-1cce9c050b14406a8f07b8b45cbba30f");
+    }
+
+    public void AcceptTerms()
+    {
+
+
+    }
+
+    public void DeclineTerms()
+    {
+        
+    }
+}
